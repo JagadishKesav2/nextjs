7b61c08d06e3fe3a0901f16420a01192bd50e0ae
jagadishkesav <s.jagadishkpd@gmail.com>
2021-07-21 19:30:47 +0530

    converted to typescript and test script code is written in typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,19 @@
-// import App from 'next/app'
+// import App from "next/app";
+import type { AppProps /*, AppContext */ } from 'next/app'
 
-const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
 
 // Only uncomment this method if you have blocking data requirements for
 // every single page in your application. This disables the ability to
 // perform automatic static optimization, causing every page in your app to
 // be server-side rendered.
 //
-// MyApp.getInitialProps = async ctx => {
+// MyApp.getInitialProps = async (appContext: AppContext) => {
 //   // calls page's `getInitialProps` and fills `appProps.pageProps`
-//   const appProps = await App.getInitialProps(ctx)
-//
+//   const appProps = await App.getInitialProps(appContext);
+
 //   return { ...appProps }
 // }
 
