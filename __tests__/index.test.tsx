7b3e70e7e5a565bfb595ee2056e35b2290d5968b
jagadishkesav <s.jagadishkpd@gmail.com>
2021-07-22 18:31:59 +0530

    update

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -3,8 +3,12 @@ import { shallow } from 'enzyme';
 import Index from '../pages/index';
 
 describe("Test case for index.js", () => {
-    test("check Next.js 100 is exists", () => {
+    test.concurrent("check Next.js 100 is exists", () => {
         const wrapper = shallow(<Index />);
         expect(wrapper.find('div').text()).toEqual('Next.js 100');
+    })
+    test.concurrent("check TDD is existing as Header", () =>{
+        const wrapper = shallow(<Index />);
+        expect(wrapper.find('title').text()).toEqual('TDD with Jest');
     })
 })
